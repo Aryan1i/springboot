4d2f73e445229e25680d9fdff0cdea9da3556015
Aryan1i <aryann.gupta.1ii@gmail.com>
2026-09-06 20:03:07 +0530

    MySQL Connection Pool Demo

---
+++ JDBC-Learning/src/com/aryan/jdbc/ConnectionPooling/MySQLConnectionPoolDemo.java
@@ -0,0 +1,49 @@
+package com.aryan.jdbc.ConnectionPooling;
+
+import java.io.*;
+import java.util.*;
+
+import com.mysql.cj.jdbc.MysqlConnectionPoolDataSource;
+
+import java.sql.*;
+
+public class MySQLConnectionPoolDemo {
+
+	public static void main(String[] args) throws SQLException, IOException {
+		InputStream is = MySQLConnectionPoolDemo.class.getClassLoader().getResourceAsStream("db.properties");
+		
+		Properties properties = new Properties();
+		 
+		String url = "";
+		String username = "";
+		String password = "";
+		  
+		try {
+			properties.load(is);
+			
+			url = properties.getProperty("url");
+			username = properties.getProperty("username");
+			password = properties.getProperty("password");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		//Creating a pool of Connection objects
+		MysqlConnectionPoolDataSource datasource = new MysqlConnectionPoolDataSource();
+		
+		datasource.setUrl(url);
+		datasource.setUser(username);
+		datasource.setPassword(password);
+		
+		//Get the connection object from connection pool
+		Connection connection = datasource.getConnection();
+		System.out.println("Got connection object From connection pool");
+		
+		System.in.read(); // pause the execution till the time customer gives any input
+		
+		//closing the connection object
+		connection.close();
+		System.out.println("Sending back Connection object to Connection Pool");
+	}
+
+}
